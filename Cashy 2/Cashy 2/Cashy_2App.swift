bf6dafd6403abc3ceb85e70981bfaad5b660b506
mina0507 <Herminekahlon07@gmail.com>
2026-01-18 11:06:16 +0100

    xcode projekt/datei cash

---
+++ Cashy 2/Cashy 2/Cashy_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cashy_2App.swift
+//  Cashy 2
+//
+//  Created by TA612 on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Cashy_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
